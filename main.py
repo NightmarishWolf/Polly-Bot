e2c11d0e0b79e6747bf58c26a30e3d70f1ea25db
Remington Thomas <remi93245@gmail.com>
2026-08-12 16:38:49 -0700

    Polly Bot Initializer

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        if message.content.startswith('!wiki'):
+            await message.channel.send('https://ship-community-valrus.fandom.com/wiki/Main_Page')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(os.environ["DISCORD_TOKEN"])
